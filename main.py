f816a943b65e1849f59c0ef8ed0574a6cc14336d
JamesJoynsonEllis <jamesmscott23@gmail.com>
2021-12-06 18:19:49 +0000

    Added files to the project, main.py is terminal based  - James

---
+++ main.py
@@ -0,0 +1,15 @@
+# Python Logger by JamesJoynsonEllis
+
+def logger():
+    from datetime import datetime
+    file_path = input("File path to log to (.txt is best): ")
+    file = open(file_path, "a")
+    log = input("LOG: ")
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_date = datetime.today().strftime("%d/%m/%Y")
+    log = "<" + current_date + " " + current_time + "> " + log
+    file.write(log)
+
+logger()
+
+# This code is not to be directly used without crediting the author
